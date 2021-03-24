d3969974af7718aa2989da11a5f9fcc351401bf7
QIXIN HE <qhee0011@student.monash.edu>
2021-03-25 08:18:47 +1100

    HomePage created.

---
+++ lear/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Route path="/" component={HomePage} exact />
+      </div>
+    </Router>
+
+  );
+}
+
+export default App;
