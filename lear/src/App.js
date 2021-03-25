9add7fcd55220e9de10c814b2c056d3fc72e3b4e
QIXIN HE <qhee0011@student.monash.edu>
2021-03-26 07:31:38 +1100

    Navigation bar created.

--- lear/src/App.js
+++ lear/src/App.js
@@ -7,16 +7,21 @@ import HomePage from './pages/HomePage';
 import LitterClassicationPage from './pages/LitterClassificationPage';
 import LawPage from './pages/LawPage';
 import ContactUsPage from './pages/ContactUsPage';
+import NavBar from './NavBar';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
+        <div id="page-body">
         <Route path="/" component={HomePage} exact />
         <Route path="/litterclassification" component={LitterClassicationPage} />
         <Route path="/law" component={LawPage} />
         <Route path="/contactus" component={ContactUsPage} />
+        </div>
+        
 
       </div>
     </Router>
---
+++ lear/src/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+//a Component
+const NavBar = () => (
+    <nav>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/litterclassification">Litter Classification</Link>
+            </li>
+            <li>
+                <Link to="/law">Laws and Penalties</Link>
+            </li>
+            <li>
+                <Link to="/contactus">Contact Us</Link>
+            </li>
+        </ul>
+    </nav>
+);
+
+export default NavBar;
