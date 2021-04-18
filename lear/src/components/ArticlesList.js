4f4d7bf502c024cc899269a994669ff3d5d0ff32
QIXIN HE <qhee0011@student.monash.edu>
2021-04-18 23:50:17 +1000

    First frontend-backend connection.

--- lear/src/components/ArticlesList.js
+++ lear/src/components/ArticlesList.js
@@ -1,30 +1,54 @@
-import React from 'react';
+import React, {useState, useEffect}  from 'react';
 import { Link } from 'react-router-dom';
 import { Fragment } from 'react';
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
 //for displaying different lists of articles
-const ArticlesList = ({ articles }) => (
-    <div style={{ height: "100vh" }} >
-        <ListGroup>
-            {articles.map((article, key) => (
-                <ListGroupItem>
-                    <div style={{
-                        height: "10vh",
-                        minHeight: "70px",
-                        maxHeight: "500px"
-                    }}>
-                        <Link style={{ textDecoration: "none", color: "white" }} key={key} to={`/law/${article.Month}`} >
-                            
-                            <h4 className="article-list-item">{article.Month}</h4>
-                        </Link>
-                        <br/>
-                        <h5>   Amount of fines: {article['Number of Fines']}</h5>
-                    </div>
-                </ListGroupItem>
-            ))}
-        </ListGroup>
-    </div>
-);
+const ArticlesList = () => {
+
+    const [merchants, setMerchants] = useState([]);
+    useEffect(() => {
+        getMerchant();
+    }, []);
+    function getMerchant() {
+        fetch('http://localhost:8080')
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                setMerchants(data);
+            });
+    };
+
+//const merchantList = merchants;
+//too many rerender
+
+
+
+    return (
+        <div style={{ height: "100vh" }} >
+            
+            <ListGroup>
+                
+                {[...merchants].map((article, key) => (
+                    <ListGroupItem>
+                        <div style={{
+                            height: "10vh",
+                            minHeight: "70px",
+                            maxHeight: "500px"
+                        }}>
+                            <Link style={{ textDecoration: "none", color: "white" }} key={key} to={`/law/${article.month}`} >
+
+                                <h4 className="article-list-item">{article.month}</h4>
+                            </Link>
+                            <br />
+                            <h5>   Amount of fines: {article['number_of_fine']}</h5>
+                        </div>
+                    </ListGroupItem>
+                ))}
+            </ListGroup>
+        </div>
+    );
+}
 
 export default ArticlesList;
