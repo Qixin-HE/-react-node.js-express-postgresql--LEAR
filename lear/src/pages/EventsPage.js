2083d8312a8e7ebbce704d4fa99bb1a216a16b67
QIXIN HE <qhee0011@student.monash.edu>
2021-05-02 01:24:21 +1000

    Tooltip added to event page.
    More instruction for using the map on event page.

--- lear/src/pages/EventsPage.js
+++ lear/src/pages/EventsPage.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import DataService from "../services/lear.service";
 import { Link } from 'react-router-dom';
-import { Container, Row, Col, ButtonGroup, ToggleButton, Button } from 'react-bootstrap';
+import { Container, Row, Col, ButtonGroup, ToggleButton, Button, Popover, OverlayTrigger, Tooltip, Image } from 'react-bootstrap';
 import MapContainer from '../components/MapContainer';
 import { Last } from 'react-bootstrap/esm/PageItem';
+import { BsFillQuestionCircleFill } from "react-icons/bs";
+
 
 const EventsPage = () => {
 
@@ -21,13 +23,13 @@ const EventsPage = () => {
     useEffect(() => {
         getSyring();
     }, []);
-   const getSyring = async() => {
+    const getSyring = async () => {
         await fetch('/syring')
             .then(response => {
                 return response.json();
             })
             .then(data => {
-                 setSyring(data);
+                setSyring(data);
             });
     };
 
@@ -35,7 +37,7 @@ const EventsPage = () => {
     useEffect(() => {
         getTrap();
     }, []);
-    const getTrap = async() => {
+    const getTrap = async () => {
         await fetch('/trap')
             .then(response => {
                 return response.json();
@@ -49,7 +51,7 @@ const EventsPage = () => {
     const [directionLong, setDirectionLong] = useState(0);
 
 
-    const handleCallback = (childData) =>{
+    const handleCallback = (childData) => {
         const lat = childData.lat;
         const long = childData.lng
         setDirectionLat(lat)
@@ -57,71 +59,97 @@ const EventsPage = () => {
         console.log(lat)
         console.log(directionLat)
         console.log(externalMapLink)
-        
+
     }
 
-    const externalMapLink = 
-    "https://www.google.com.au/maps/dir/" + directionLat + "," + directionLong
+    const externalMapLink =
+        "https://www.google.com.au/maps/dir/" + directionLat + "," + directionLong
 
     const navigateToExternalMap = (url) => {
-        const newWindow = window.open( url , '_blank', 'noopener,noreferrer')
-  if (newWindow) newWindow.opener = null
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
     }
+    //add more instruction(user experience)
+    const popover = (
+        <Popover id="popover-basic">
+            <Popover.Title as="h3">How to use the map?</Popover.Title>
+            <Popover.Content>
+            <p>• You can change the makers on the map by clicking the button on <strong>Change the marker sets</strong> section.
+            </p>
+            <p>• You can <strong>click on a marker</strong> to check its detail.</p>
+            <p>• <strong>After you clicked a marker</strong>, you can click on the "<strong>check in google map website</strong>" to 
+                check current marker location's navigation information.
+            </p>
+      </Popover.Content>
+        </Popover>
+    );
+
     
-    //if (radioValue === '1' ) {
+      
+    return (
+        <>
+        
+            <h1 class="text-dark font-weight-light" style={{ paddingTop: "40px", marginBlockEnd: "30px" }}>
+                Events and related locations</h1>
 
-        return (
-            <>
-                <h1 class="text-dark font-weight-light" style={{ paddingTop: "40px", marginBlockEnd: "30px" }}>Events and related locations</h1>
 
-                <Container style={{
-                    height: "100%",
-                    width: "100%",
-                    maxWidth: "100%"
-                }}>
-                    <Row>
-                        <Col sm={10} style={{ height: "900px" }}>
+            <Container style={{
+                height: "100%",
+                width: "100%",
+                maxWidth: "100%"
+            }}>
+                <Row >
+                    <Col sm={10} style={{ height: "900px" }}>
+                        <Row style={{ margin: "10px"}}>
+                        <h5 style={{ textAlign: "left" }}>Interactive map <span />
+                           
+                            <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+                                <BsFillQuestionCircleFill />
+                            </OverlayTrigger>
+                            
+                        </h5> <h5 style={{color: "grey", textAlign: "left"}}><span>&nbsp;&nbsp;</span>(click the <BsFillQuestionCircleFill /> to get more info)</h5>
+                        </Row>
 
-                          
-                        {radioValue === '1' && syring.length > 0 && trap.length > 0 ? <MapContainer locations={[...syring]} parentCallback = {handleCallback}/> : null}
-                        {radioValue === '2'&& syring.length > 0 && trap.length > 0 ? <MapContainer locations={[...trap]} parentCallback = {handleCallback}/> : null }
-                         
 
-                        </Col>
-                        <Col sm={2} style={{ marginLeft: "-30px" }}>
+                        {radioValue === '1' && syring.length > 0 && trap.length > 0 ? <MapContainer locations={[...syring]} parentCallback={handleCallback} /> : null}
+                        {radioValue === '2' && syring.length > 0 && trap.length > 0 ? <MapContainer locations={[...trap]} parentCallback={handleCallback} /> : null}
 
-                            <Row style={{ marginBottom: "40px" }}>
-                                <ButtonGroup toggle>
-                                    {radios.map((radio, idx) => (
-                                        <ToggleButton
-                                            key={idx}
-                                            type="radio"
-                                            variant="info"
-                                            name="radio"
-                                            value={radio.value}
-                                            checked={radioValue === radio.value}
-                                            onChange={(e) => setRadioValue(e.currentTarget.value)}
-                                        >
-                                            {radio.name}
-                                        </ToggleButton>
-                                    ))}
-                                </ButtonGroup>
-                            </Row>
-                            <Row>
-                                <h5>Get to this location:</h5>
-                                <Button variant="success" size="lg" onClick={() => navigateToExternalMap(externalMapLink)}>
-                                    Check in google map website
+
+                    </Col>
+                    <Col sm={2} style={{ marginLeft: "-30px" }}>
+
+                        <Row style={{ marginBottom: "40px" }}>
+                            <ButtonGroup toggle>
+                                {radios.map((radio, idx) => (
+                                    <ToggleButton
+                                        key={idx}
+                                        type="radio"
+                                        variant="info"
+                                        name="radio"
+                                        value={radio.value}
+                                        checked={radioValue === radio.value}
+                                        onChange={(e) => setRadioValue(e.currentTarget.value)}
+                                    >
+                                        {radio.name}
+                                    </ToggleButton>
+                                ))}
+                            </ButtonGroup>
+                        </Row>
+                        <Row>
+                            <h5>Get to this location:</h5>
+                            <Button variant="success" size="lg" onClick={() => navigateToExternalMap(externalMapLink)}>
+                                Check in google map website
     </Button>{' '}
 
-                            </Row>
-                        </Col>
-                    </Row>
-                </Container>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
 
+            
+        </>
 
-            </>
-
-        );
+    );
 
 }
 
